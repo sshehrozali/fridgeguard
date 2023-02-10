0166470f351343e3949cfb60095f43227a3cb0f8
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 10:01:36 +0000

    producerConfig: Created new method Implemented method body returned props

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaProducerConfig.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaProducerConfig.java
@@ -1,10 +1,24 @@
 package com.blakbear.bearcub.sensor.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Configuration
 public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
 }
