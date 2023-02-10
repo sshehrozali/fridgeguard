069d6b22e1907d061342fc14476edc350cb291f6
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 09:46:29 +0000

    SensorAlertController: Created new POST method newAlertRequest accepting NewSensorAlertRequest as request body

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertController.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertController.java
@@ -1,10 +1,17 @@
 package com.blakbear.bearcub.sensor;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/v1")
 @RequiredArgsConstructor
 public class SensorAlertController {
     private final SensorAlertProducerService sensorAlertProducerService;
+
+    @PostMapping("/new/alert")
+    void newAlertRequest(@RequestBody NewSensorAlertRequest newSensorAlertRequest) {
+        sensorAlertProducerService.produceNewAlert(newSensorAlertRequest);
+    }
 }
