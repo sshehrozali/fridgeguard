830560becef69054de89d189896bf1fe10f91b06
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 10:05:41 +0000

    producerConfig: Added @Bean kafkaTemplate: Created new method Dependency injected ProducerFactory<String, String> Returned KafkaTemplate<>(producerFactory) Added @Bean

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaProducerConfig.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaProducerConfig.java
@@ -3,8 +3,10 @@ package com.blakbear.bearcub.sensor.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
@@ -24,7 +26,13 @@ public class KafkaProducerConfig {
         return props;
     }
 
+    @Bean
     public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
