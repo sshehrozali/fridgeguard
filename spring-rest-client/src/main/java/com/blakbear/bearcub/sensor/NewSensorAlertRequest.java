1fbe32e1ad08696beaaddfc96b1db10942818352
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 09:43:29 +0000

    SensorAlertProducerService: Created new method produceNewAlert that will produce a message to subscribed Kafka topic

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/NewSensorAlertRequest.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/NewSensorAlertRequest.java
@@ -1,2 +1,13 @@
-package com.blakbear.bearcub.sensor;public record NewSensorAlertRequest() {
+package com.blakbear.bearcub.sensor;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record NewSensorAlertRequest(
+        String batchName,
+        String bearcub,
+        String alertReceivedAt
+) {
 }
