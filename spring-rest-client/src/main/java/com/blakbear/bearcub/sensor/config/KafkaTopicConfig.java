d625e3e789c6978d03263344ecd17f1c09b5e02e
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 09:54:35 +0000

    alertsTopic: Created new Bean for returning new Kafka topic named as alerts

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaTopicConfig.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaTopicConfig.java
@@ -1,7 +1,16 @@
 package com.blakbear.bearcub.sensor.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+    @Bean
+    public NewTopic alertsTopic() {
+        return TopicBuilder
+                .name("alerts")
+                .build();
+    }
 }
