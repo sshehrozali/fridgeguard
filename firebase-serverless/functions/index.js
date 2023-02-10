f767d410c375b45246f92d554fef722b736d05fc
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 11:33:06 +0000

    updateSensorData: Implemetned Axios POST request to Spring Kafka REST Client for aggregating log in topic

--- firebase-serverless/functions/index.js
+++ firebase-serverless/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const { BigQuery } = require("@google-cloud/bigquery");
+const axios = require("axios");
 
 exports.createSensorData = functions.firestore
   .document("/data/{documentId}")
@@ -24,6 +25,33 @@ exports.updateSensorData = functions.firestore
       "Spoiled Food Detected. Exporting data to BigQuery..."
     );
 
+    // Instantiate a new current timestamp
+    const timestamp = new Date();
+
+    // Make Axios POST request to Spring Kafka Client
+    // URL: http://localhost:8080/api/v1/alerts/new
+    // Produce new alert in Kafka topic
+    // Return back
+    // Continue perform write stream to BigQuery
+    const options = {
+      method: "POST",
+      url: "http://localhost:8080/api/v1/alerts/new",
+      data: {
+        batchName: data.batch_name,
+        bearcub: data.bearcub,
+        alertReceivedAt: timestamp.toLocaleString(),
+      },
+    };
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
+    // Start stream operation to Google Cloud BigQuery
     // Create new BigQuery instance
     const bigquery = new BigQuery({
       keyFilename: "key.json",
@@ -44,14 +72,14 @@ exports.updateSensorData = functions.firestore
       spoiled: data.spoiled,
     };
 
+    // Data to be exported
     console.log(cookedData);
-
-    const stream = [cookedData];
     try {
-      // Insert into BigQuery
+      // Insert into BigQuery -> Write stream operation
       await bigquery.dataset("data").table("sensor").insert(cookedData);
       functions.logger.info("Data successfully exported");
     } catch (error) {
+      // On error, log it
       functions.logger.error("Error while exporting data to BigQuery");
       functions.logger.error(error);
     }
