4dd7edd9b01ebda0b40cb2281b408d9a518e52b5
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 10:37:59 +0000

    SensorAlertProducerService: Injected KafkaTemplate

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertProducerService.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertProducerService.java
@@ -1,9 +1,13 @@
 package com.blakbear.bearcub.sensor;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SensorAlertProducerService {
+    private KafkaTemplate<String, String> kafkaTemplate;
     public void produceNewAlert(NewSensorAlertRequest newSensorAlertRequest) {
     }
 }
