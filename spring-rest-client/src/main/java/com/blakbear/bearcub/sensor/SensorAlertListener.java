e5e37695aacdca9b18c93c3f7f4f8bcd50971539
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 10:29:16 +0000

    listener: Added @KafkaListener Added @Slf4j to SensorAlertListener

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertListener.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertListener.java
@@ -1,9 +1,14 @@
 package com.blakbear.bearcub.sensor;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SensorAlertListener {
+    @KafkaListener(topics = "alerts")
     public void listener() {
 
     }
