d3c4f0b26c61cba284e74bda237c80996f4bc5fb
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 09:57:55 +0000

    KafkaProducerConfig: Added field for Kafka servers

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaProducerConfig.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/config/KafkaProducerConfig.java
@@ -1,7 +1,10 @@
 package com.blakbear.bearcub.sensor.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 }
