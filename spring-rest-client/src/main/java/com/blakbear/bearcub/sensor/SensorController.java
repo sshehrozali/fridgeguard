f5421aa8f74919fedd599183955dd6f2092783f5
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 09:36:05 +0000

    SensorController: Injected SensorAlertProducerService

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorController.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorController.java
@@ -1,7 +1,10 @@
 package com.blakbear.bearcub.sensor;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/v1")
+@RequiredArgsConstructor
 public class SensorController {
+    private final SensorAlertProducerService sensorAlertProducerService;
 }
