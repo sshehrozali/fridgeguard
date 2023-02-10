475fd2c75b7705f40be55cf18deeb55880ad0776
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-10 10:47:06 +0000

    SensorAlertController: Added logging SensorAlertController: Added @RequestMapping SensorAlertController: Removed argument to @RestController

--- spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertController.java
+++ spring-rest-client/src/main/java/com/blakbear/bearcub/sensor/SensorAlertController.java
@@ -1,17 +1,22 @@
 package com.blakbear.bearcub.sensor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1")
+@RestController
+@RequestMapping("api/v1/alerts")
 @RequiredArgsConstructor
+@Slf4j
 public class SensorAlertController {
     private final SensorAlertProducerService sensorAlertProducerService;
 
-    @PostMapping("/new/alert")
+    @PostMapping("new")
     void newAlertRequest(@RequestBody NewSensorAlertRequest newSensorAlertRequest) {
+        log.info("New Sensor Alert Request received");
         sensorAlertProducerService.produceNewAlert(newSensorAlertRequest);
     }
 }
